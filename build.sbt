b4e645cc89c7219fe0b1e68de323a004168075df
CasperKoning <casper.e.koning@gmail.com>
2015-10-02 00:02:49 +0200

    First implementation of decision tree model with cross validation and test implementation of display method

--- build.sbt
+++ build.sbt
@@ -4,17 +4,14 @@ version := "1.0"
 
 scalaVersion := "2.10.5"
 
-val sparkCore = "org.apache.spark" % "spark-core_2.10" % "1.4.0"
-val sparkSql = "org.apache.spark" % "spark-sql_2.10" % "1.4.0"
-val sparkMlLib = "org.apache.spark" % "spark-mllib_2.10" % "1.4.0"
-val sparkStreaming = "org.apache.spark" % "spark-streaming_2.10" % "1.4.0"
-
-val sparkCsv = "com.databricks" % "spark-csv_2.10" % "1.1.0"
+val sparkCore = "org.apache.spark" % "spark-core_2.10" % "1.5.1"
+val sparkSql = "org.apache.spark" % "spark-sql_2.10" % "1.5.1"
+val sparkMlLib = "org.apache.spark" % "spark-mllib_2.10" % "1.5.1"
+val sparkStreaming = "org.apache.spark" % "spark-streaming_2.10" % "1.5.1"
 
 libraryDependencies ++= Seq(
   sparkCore,
   sparkSql,
   sparkMlLib,
-  sparkStreaming,
-  sparkCsv
+  sparkStreaming
 )
--- src/main/scala/nl/ordina/bigdata/myo/MyoMain.scala
+++ src/main/scala/nl/ordina/bigdata/myo/MyoMain.scala
@@ -1,7 +1,12 @@
 package nl.ordina.bigdata.myo
 
-import org.apache.spark.mllib.tree.model.DecisionTreeModel
-import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType}
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
+import org.apache.spark.ml.tuning.{CrossValidator, CrossValidatorModel, ParamGridBuilder}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.types.{DoubleType, StructField, StructType}
 import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
@@ -12,16 +17,17 @@ object MyoMain {
     //Initialize contexts
     val conf = new SparkConf().setMaster("local[6]").setAppName("myo-streaming")
     val sc = new SparkContext(conf)
-    val sqlContext = new SQLContext(sc)
+    implicit val sqlContext = new SQLContext(sc)
     val streamingContext = new StreamingContext(sc, Seconds(1))
 
     //Start training part
     val dataFrame = createDataFrame(Constants.dataPath, sc, sqlContext)
-    implicit val model: DecisionTreeModel = trainModel(dataFrame)
+    implicit val schema = dataFrame.schema
+    implicit val model = trainModel(dataFrame)
 
     //Start streaming part
     val dstream = streamingContext.socketTextStream("localhost", Constants.dataServerPort)
-    dstream.foreachRDD(rdd => rdd.foreach(displayPrediction))
+    dstream.foreachRDD(rdd => displayPrediction(rdd))
     streamingContext.start()
     streamingContext.awaitTermination()
   }
@@ -29,39 +35,49 @@ object MyoMain {
   def createDataFrame(path: String, sc: SparkContext, sqlContext: SQLContext): DataFrame = {
     val data = sc.textFile(path)
     val headerPart = data.first()
-    val dataPart = data.filter(line => !line.startsWith("mean"))
+    val dataPart = data.filter(line => !line.startsWith("label")) //Want to leave out the header, so we filter it out
     val schema = determineSchemaFromHeader(headerPart)
-    val rowRDD = dataPart.map(line => Row(line.split(";"))) //Want to leave out the header, so we filter it out
+    val rowRDD = dataPart.map(toMyoRow)
     sqlContext.createDataFrame(rowRDD, schema)
   }
 
   def determineSchemaFromHeader(header: String): StructType = {
     val splits = header.split(";")
-    val label = splits.reverse.head //last entry
-    val features = splits.reverse.tail
-
-    val labelStruct = StructField(label, StringType, nullable = false)
-    val featuresStruct = features.map(featureName => StructField(featureName, DoubleType, nullable = false)) //all doubles
-    StructType((labelStruct :: featuresStruct.toList).reverse)
+    StructType(splits.map(name => StructField(name, DoubleType, nullable = false)))
   }
 
-  //TODO
-  /**
-   *
-   *
-   * This method should train a decision tree model. Currently, this has no implementation.
-   */
-  def trainModel(dataFrame: DataFrame): DecisionTreeModel = {
-    null
+  def toMyoRow(line: String): Row = {
+    val splits = line.split(";")
+    Row.fromSeq(splits.map(_.toDouble))
   }
 
-  //TODO
+  def trainModel(dataFrame: DataFrame): CrossValidatorModel = {
+    val labelIndexer = new StringIndexer().setInputCol("label").setOutputCol("indexedLabel").fit(dataFrame)
+    val assembler = new VectorAssembler().setInputCols(Constants.featureCols).setOutputCol("features")
+    val dt = new DecisionTreeClassifier().setLabelCol("indexedLabel").setFeaturesCol("features").setPredictionCol("prediction").setImpurity("gini")
+    val pipeline = new Pipeline().setStages(Array(labelIndexer, assembler, dt))
+
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(dt.maxBins, Array(5, 10, 20))
+      .addGrid(dt.maxDepth, Array(5, 10, 20))
+      .build()
+
+    val cv = new CrossValidator()
+      .setEstimator(pipeline)
+      .setEvaluator(new MulticlassClassificationEvaluator().setLabelCol("indexedLabel").setPredictionCol("prediction"))
+      .setEstimatorParamMaps(paramGrid)
+      .setNumFolds(10)
+
+    cv.fit(dataFrame)
+  }
+
   /**
-   * This method should display the prediction using some form of output. Currently, it just prints the label as a test
-   * implementation.
+   * This method should display the prediction using some form of output. Currently, it just prints the predicted label
+   * as a test implementation to the standard out.
    */
-  def displayPrediction(line: String)(implicit model: DecisionTreeModel): Unit = {
-    val label = line.split(";").reverse.head
-    println(label)
+  def displayPrediction(rdd: RDD[String])(implicit model: CrossValidatorModel, sqlContext: SQLContext, schema: StructType): Unit = {
+    val dataFrame = sqlContext.createDataFrame(rdd.map(toMyoRow),schema)
+    val predictionDataFrame = model.transform(dataFrame)
+    predictionDataFrame.foreach(row => println(s"prediction is ${row.get(row.fieldIndex("prediction"))}"))
   }
 }
