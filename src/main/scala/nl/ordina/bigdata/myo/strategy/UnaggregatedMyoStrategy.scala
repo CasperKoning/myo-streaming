d3fb84a630257c2486db40f3821ffcfb9abc305f
CasperKoning <casper.e.koning@gmail.com>
2015-10-30 17:41:47 +0100

    strategy uses new data

--- src/main/scala/nl/ordina/bigdata/myo/strategy/UnaggregatedMyoStrategy.scala
+++ src/main/scala/nl/ordina/bigdata/myo/strategy/UnaggregatedMyoStrategy.scala
@@ -4,12 +4,12 @@ import nl.ordina.bigdata.myo.Constants
 import org.apache.spark.SparkContext
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
+import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.regression.DecisionTreeRegressor
 import org.apache.spark.ml.tuning.{CrossValidator, CrossValidatorModel, ParamGridBuilder}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.types.StructType
-import org.apache.spark.sql.{SQLContext, DataFrame}
+import org.apache.spark.sql.{DataFrame, SQLContext}
 
 class UnaggregatedMyoStrategy extends MyoStrategy {
   private var schema: StructType = null
@@ -20,22 +20,24 @@ class UnaggregatedMyoStrategy extends MyoStrategy {
     dataFrame
   }
 
-
   override def trainModel(dataFrame: DataFrame): CrossValidatorModel = {
-    val labelIndexer = new StringIndexer().setInputCol(Constants.LABEL).setOutputCol("indexedLabel").fit(dataFrame)
-
+    dataFrame.cache()
     val assembler = new VectorAssembler().setInputCols(Constants.FEATURES).setOutputCol("features")
-    val dt = new DecisionTreeRegressor().setLabelCol("indexedLabel").setFeaturesCol("features").setPredictionCol("prediction")
-    val pipeline = new Pipeline().setStages(Array(labelIndexer, assembler, dt))
+    val dt = new DecisionTreeRegressor()
+      .setLabelCol(Constants.LABEL)
+      .setFeaturesCol("features")
+      .setPredictionCol("prediction")
+      .setImpurity("variance")
+
+    val pipeline = new Pipeline().setStages(Array(assembler, dt))
 
     val paramGrid = new ParamGridBuilder()
-      .addGrid(dt.maxBins, Array(5, 10, 25))
-      .addGrid(dt.maxDepth, Array(5, 10, 25))
+      .addGrid(dt.maxDepth, Array(10,20,30))
       .build()
 
     val cv = new CrossValidator()
       .setEstimator(pipeline)
-      .setEvaluator(new MulticlassClassificationEvaluator().setLabelCol("indexedLabel").setPredictionCol("prediction"))
+      .setEvaluator(new MulticlassClassificationEvaluator().setLabelCol(Constants.LABEL).setPredictionCol("prediction"))
       .setEstimatorParamMaps(paramGrid)
       .setNumFolds(3)
 
@@ -44,8 +46,8 @@ class UnaggregatedMyoStrategy extends MyoStrategy {
 
   override def displayPrediction(rdd: RDD[String], model: CrossValidatorModel, sqlContext: SQLContext): Unit = {
     val dataFrame = sqlContext.read.schema(schema).json(rdd)
-    val predictionDataFrame = model.transform(dataFrame)
-    predictionDataFrame.groupBy("prediction").count().show()
+    val predictionsDataFrame = model.transform(dataFrame)
+    predictionsDataFrame.select("prediction").show()
   }
 
 }
