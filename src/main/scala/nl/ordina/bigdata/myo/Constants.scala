cf14a61d73ee9c222ea60c0fbfab665a33b7d1b1
CasperKoning <casper.e.koning@gmail.com>
2015-10-30 17:40:47 +0100

    correct features and correct port for data

--- src/main/scala/nl/ordina/bigdata/myo/Constants.scala
+++ src/main/scala/nl/ordina/bigdata/myo/Constants.scala
@@ -2,7 +2,7 @@ package nl.ordina.bigdata.myo
 
 object Constants {
   val LABEL = "label"
-  val FEATURES = Array("acceleratorMeter","emg","eulerAngles","gyro","normalizedAcceleratorMeter","normalizedEulerAngles","normalizedGyro","normalizedQuaternion","quaternion")
+  val FEATURES = Array("emg_0","emg_1","emg_2","emg_3","emg_4","emg_5","emg_6","emg_7","normalizedQuaternion_x","normalizedQuaternion_y","normalizedQuaternion_z","normalizedQuaternion_w","quaternion_x","quaternion_y","quaternion_z","quaternion_w","acceleratorMeter_x","acceleratorMeter_y","acceleratorMeter_z","gyro_x","gyro_y","gyro_z","normalizedAcceleratorMeter_x","normalizedAcceleratorMeter_y","normalizedAcceleratorMeter_z","normalizedGyro_x","normalizedGyro_y","normalizedGyro_z","eulerAngels_pitch","eulerAngels_roll","eulerAngels_yaw","normalizedEulerAngels_pitch","normalizedEulerAngels_roll","normalizedEulerAngels_yaw")
   val DATA_PATH = "D:/dev/code/myo-streaming/src/main/resources"
-  val DATA_SERVER_PORT = 9999
+  val DATA_SERVER_PORT = 5555
 }
